0b82ef3620eb7a27889861bd8608352f8bcb33e4
Alina-d87 <78098869+Alina-d87@users.noreply.github.com>
2022-07-24 14:26:27 +0000

    Deploying to gh-pages from @ Alina-d87/goit-js-hw-08@464182e4491d959713388afa6a42c133c9a9e7ec 🚀

--- 03-feedback.4be9470e.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={input:document.querySelector("input"),textarea:document.querySelector("textarea"),button:document.querySelector("button"),form:document.querySelector(".feedback-form")};console.log(e),e.form.addEventListener("submit",(function(t){t.preventDefault();var o={email:"".concat(e.input.value),message:"".concat(e.textarea.value)};console.log(o),function(e){JSON.stringify(e);localStorage.setItem("feedback-form-state",JSON.stringify(e))}(o),e.input.value="",e.textarea.value=""}))}();
-//# sourceMappingURL=03-feedback.4be9470e.js.map
