48afc2d975b7ac6a044535a7123865fd77539053
Alina Domnich <78098869+Alina-d87@users.noreply.github.com>
2022-07-24 21:05:56 +0300

    change hw

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,21 +1,19 @@
-//import Player from '@vimeo/player';
+import Player from '@vimeo/player';
 import _throttle from 'lodash';
 
-const refVideo = document.querySelector('#vimeo-player');
-console.log(refVideo);
-
-const player = new Vimeo.Player(refVideo);
-console.log(player);
+const player = new Player('handstick', {
+  id: 'vimeo-player',
+  width: 640,
+});
 
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-player
-  .on('timeupdate', function (time) {
-    const timeSecond = time.seconds;
-    console.log(timeSecond);
-    const saveTime = localStorage.setItem(LOCALSTORAGE_KEY, timeSecond);
-  })
-  .throttle(1000);
+player.on('timeupdate', function (time) {
+  const timeSecond = time.seconds;
+  console.log(timeSecond);
+  const saveTime = localStorage.setItem(LOCALSTORAGE_KEY, timeSecond);
+  const throobleTime = _.throttle(saveTime, 1000);
+});
 
 const localTime = localStorage.getItem(LOCALSTORAGE_KEY);
 
