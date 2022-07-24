37adb47a02cd1467cafa2ab422cb045e135406cc
Alina Domnich <78098869+Alina-d87@users.noreply.github.com>
2022-07-25 00:06:21 +0300

    add 3hw

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,49 @@
-//подія інпут прослухоування
-//провіряти стан сховища: якщо є дані заповнюємо ними поля, ні - поля порожні
-//при сабміті форми очистити сховище ш поля форми
-//додати lodash.throtle для оновлення локального сховища 500мс
+import _throttle from 'lodash';
 
-const ref = {
-  input: document.querySelector('input'),
-  textarea: document.querySelector('textarea'),
-  form: document.querySelector('.feedback-form'),
-};
-console.log(ref);
-
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
-
+console.log(form.elements);
 outputForm();
 
-ref.form.addEventListener('submit', localInput);
+form.addEventListener('input', saveDataForm);
+textarea.addEventListener('submit', onTextInput);
 
-function localInput(event) {
-  event.preventDefault();
-  const value = {
-    email: `${ref.input.value}`,
-    message: `${ref.textarea.value}`,
+function saveDataForm(event) {
+  const dataForm = {
+    email: `${form.elements.target}`,
+    message: `${textarea.value}`,
   };
-  console.log(value);
-  const localList = localStorage.getItem('LOCALSTORAGE_KEY');
-  const listData = localList ? JSON.parse(localList) : [];
-  localStorage.setItem('LOCALSTORAGE_KEY', JSON.stringify(value));
+  console.log(dataForm);
+  localStorage.setItem(LOCALSTORAGE_KEY, dataForm);
   outputForm();
-  ref.form.reset();
 }
 
 function outputForm() {
-  ref.form.value = localStorage.getItem(LOCALSTORAGE_KEY) || '';
+  form.value = localStorage.getItem(LOCALSTORAGE_KEY) || '';
 }
+
+function onTextInput(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+//outputForm();
+
+//ref.form.addEventListener('submit', localInput);
+
+//function localInput(event) {
+//  event.preventDefault();
+//  const value = {
+//    email: `${input.value}`,
+//    message: `${textarea.value}`,
+//  };
+//  console.log(value);
+//  const localList = localStorage.getItem('LOCALSTORAGE_KEY');
+//  const listData = localList ? JSON.parse(localList) : [];
+//  localStorage.setItem('LOCALSTORAGE_KEY', JSON.stringify(value));
+//  outputForm();
+//  ref.form.reset();
+//}
